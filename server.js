8dea9706151920449fa7d0b31a3dbc307c41b890
Nahid <nahid@Nahids-MacBook-Air.local>
2026-02-06 09:23:35 -0800

    Fix Node import syntax for Railway

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-import express from "express";
-import cors from "cors";
-import bodyParser from "body-parser";
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
 
 const app = express();
 
